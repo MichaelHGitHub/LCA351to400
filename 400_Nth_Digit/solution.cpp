2bda51d4ec860f2ae1c9b92f2c5a17d8bcf4e176
Kindsword <kindsword@live.com>
2023-02-12 21:41:07 -0500

    Add comments to 400

--- 400_Nth_Digit/solution.cpp
+++ 400_Nth_Digit/solution.cpp
@@ -1,11 +1,11 @@
 #include "header.h"
 
 int findNthDigit(int n) {
-
     int result = 0;
     int phase = 1;
     int start = 1;
     int end = 9;
+    // digis is the the number of total digits the current phase provides.
     long long digis = (end - start + 1) * phase;
 
     while (n >= digis) {
@@ -16,17 +16,21 @@ int findNthDigit(int n) {
         digis = (long long)(end - start + 1) * phase;
     }
 
-    start /= 10;
+    // Back to the end of the previous phase
     end /= 10;
 
+    // d is the index of digits in the final number we need.
     int d = n % phase;
+    // num is the number pre to the final number that contains the digit we need
     int num = n / phase + end;
 
     if (d == 0) {
         result = num % 10;
     }
     else {
+        // ++num has the digit we need
         ++num;
+        // now extract the digit we need
         for (int i = phase; i >= d; i--) {
             result = num % 10;
             num /= 10;
